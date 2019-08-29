cf4f68b31c90db3b69da839f04a0435eb986200c
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-08-28 19:33:34 -0700

    I think I am all set up?

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 const storeName = 'Pandas';
+
 document.title = `${storeName} Grocery Store`;
-console.log('what is in document:',document.all[23]);
+
+console.log('what is in document:',document.all[24]);
