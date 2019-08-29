feec0695cf8c6f5ddda5f7a6798b0d3e32e47a40
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-08-28 19:51:20 -0700

    somewhere in jacques point

--- js/app.js
+++ js/app.js
@@ -2,4 +2,13 @@ const storeName = 'Pandas';
 
 document.title = `${storeName} Grocery Store`;
 
-console.log('what is in document:',document.all[24]);
+console.log('what is in document:',document.all);
+
+console.log('tag element:',document.getElementsByTagName('p'));
+
+let h1_OfHeader = document.getElementById('mainIntro');
+// console.log(h1_OfHeader); checking
+h1_OfHeader.innerHTML = 'Welcome!';
+
+const getAllNavListClasses = document.querySelectorAll('.nav_list_item');
+console.log(getAllNavListClasses);
