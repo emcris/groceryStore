6c2637156616da9f9c4237649bcc4f9738b3b264
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-08-28 20:13:16 -0700

    adding an img we are doing something with it

--- js/app.js
+++ js/app.js
@@ -12,3 +12,14 @@ h1_OfHeader.innerHTML = 'Welcome!';
 
 const getAllNavListClasses = document.querySelectorAll('.nav_list_item');
 console.log(getAllNavListClasses);
+
+const getIconCredit = document.querySelector('#iconsCredit');
+const testDifference = document.getElementById('iconsCredit');
+console.log('get the id:',getIconCredit);
+console.log('get the id:',testDifference);
+
+const example = document.querySelector('.nav_list_item');
+console.log(example);
+
+const getClasses = document.getElementsByClassName('nav_list_item');
+console.log('get the class:',getClasses);
